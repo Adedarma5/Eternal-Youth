5dc6c2017edcf3ed0c5abf466aeb4a3470e88ec9
Rantau Himawan <88751108+pharaohpants@users.noreply.github.com>
2023-11-23 04:14:50 +0700

    Update NavbarComponents.jsx

--- EternalYouth/src/components/NavbarComponents.jsx
+++ EternalYouth/src/components/NavbarComponents.jsx
@@ -5,14 +5,14 @@ const NavbarComponents = () => {
     <div>
         <Navbar xpand="lg" >
       <Container>
-        <img src="public/logo.png"></img>
+        <img className="logo" src="/Logo.png"></img>
         <Navbar.Toggle aria-controls="basic-navbar-nav"  />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav-link" > 
-          <Nav.Link href="Homepage">Home</Nav.Link>
+          <Nav.Link href="Homepage">Beranda</Nav.Link>
           <Nav.Link href="Layanankonsultasi">Layanan Konsultasi</Nav.Link>
           <Nav.Link href="Pemesanan">Pemesanan</Nav.Link>
-          <NavDropdown title="Informasi" id="basic-nav-dropdown" >
+          <NavDropdown title="Informasi" id="basic-navbar-nav" >
               <NavDropdown.Item href="TentangKami">Tentang Kami</NavDropdown.Item>
               <NavDropdown.Item href="PusatBantuan">Pusat Bantuan</NavDropdown.Item>
             </NavDropdown>
@@ -22,12 +22,12 @@ const NavbarComponents = () => {
             <Form.Control
               type="text"
               placeholder="Cari sesuatu"
-              className=" search"
+              className="search"
               />
               </Col>
               </Row>
-              <input type="image" src="/login.png" alt="pemesanan" className="btn-login" href="/pages/pemesanan"></input>
-              <input type="image" src="/keranjang.png" alt="pemesanan" className="btn-keranjang" href="/pages/pemesanan"></input>
+              <input type="image" src="/Login.png" alt="pemesanan" className="btn-login" href="/pages/pemesanan"></input>
+              <input type="image" src="/Keranjang.png" alt="pemesanan" className="btn-keranjang" href="/pages/pemesanan"></input>
               {/* <img src="public/keranjang.png" className="btn-keranjang"></img> */}
           </Nav>
 
