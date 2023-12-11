4f2d95299ce841584e0e040d2ef03fb54663996a
Adedarma5 <151549837+Adedarma5@users.noreply.github.com>
2023-12-11 13:12:45 +0700

    Add files via upload

--- EternalYouth/src/components/NavbarComponents.jsx
+++ EternalYouth/src/components/NavbarComponents.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Container, Nav, NavDropdown, Row, Form, Col } from "react-bootstrap"
-
+import { Icon } from '@iconify/react';
 const NavbarComponents = () => {
   return (
     <div>
@@ -8,13 +8,13 @@ const NavbarComponents = () => {
         <img className="logo" src="/Logo.png"></img>
         <Navbar.Toggle aria-controls="basic-navbar-nav"  />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav-link" > 
-          <Nav.Link href="Homepage">Beranda</Nav.Link>
-          <Nav.Link href="Layanankonsultasi">Layanan Konsultasi</Nav.Link>
-          <Nav.Link href="Pemesanan">Pemesanan</Nav.Link>
-          <NavDropdown title="Informasi" id="basic-navbar-nav" >
-              <NavDropdown.Item href="TentangKami">Tentang Kami</NavDropdown.Item>
-              <NavDropdown.Item href="PusatBantuan">Pusat Bantuan</NavDropdown.Item>
+          <Nav className="nav-link"  > 
+          <Nav.Link className="beranda" href="Homepage" style={{color: '#FFD26C'}}>Beranda</Nav.Link>
+          <Nav.Link href="Layanankonsultasi"style={{color: '#FFD26C'}}>Layanan Konsultasi</Nav.Link>
+          <Nav.Link href="Pemesanan"style={{color: '#FFD26C'}}>Pemesanan</Nav.Link>
+          <NavDropdown title="Informasi" id="basic-navbar-nav" className="text-primary">
+              <NavDropdown.Item href="TentangKami"style={{color: '#FFD26C'}}>Tentang Kami</NavDropdown.Item>
+              <NavDropdown.Item href="PusatBantuan"style={{color: '#FFD26C'}}>Pusat Bantuan</NavDropdown.Item>
             </NavDropdown>
             
         <Row>
@@ -26,8 +26,8 @@ const NavbarComponents = () => {
               />
               </Col>
               </Row>
-              <input type="image" src="/Login.png" alt="pemesanan" className="btn-login" href="/pages/pemesanan"></input>
-              <input type="image" src="/Keranjang.png" alt="pemesanan" className="btn-keranjang" href="/pages/pemesanan"></input>
+              <Icon icon="mdi:account-circle-outline" color="#cfc249" width="40" height="40"href="/pages/pemesanan" className="btn-login"/>
+              <Icon icon="mdi:cart-variant" color="#cfc249" width="40" height="40"href="/pages/pemesanan" className="btn-keranjang"/>
               {/* <img src="public/keranjang.png" className="btn-keranjang"></img> */}
           </Nav>
 
