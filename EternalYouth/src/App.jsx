07d8eac800c2aaa7606d5abbace517fe3638b82d
Adedarma5 <151549837+Adedarma5@users.noreply.github.com>
2023-12-11 13:11:56 +0700

    Add files via upload

--- EternalYouth/src/App.jsx
+++ EternalYouth/src/App.jsx
@@ -1,18 +1,22 @@
 import {Routes, Route} from 'react-router-dom'
-
-// import FooterComponents from './components/FooterComponents';
 import NavbarComponents from './components/NavbarComponents';
 
 import Homepage from './pages/Homepage'
 import Layanankonsultasi from './pages/Layanankonsultasi';
 import Pemesanan from './pages/Pemesanan';
-import Pusatbantuan from './pages/Pusatbantuan';
 import Tentangkami from './pages/Tentangkami';
 import Informasi from './pages/Informasi';
 import Kalkulator from './pages/Kalkulator';
-import Payment from './pages/Payment';
-import LoginPage from './pages/LoginPage';
+import FooterComponents from './components/FooterComponents'
+import Pusatbantuan from './pages/Pusatbantuan';
+import LoginSignUpPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import Payment from './pages/Payment';
+import Profil from './pages/profil';
+import Chat from './pages/Chat';
+import Keranjang from './pages/Keranjang';
+
+
 
 
 
@@ -30,13 +34,16 @@ function App() {
           <Route path="/Layanankonsultasi" Component={Layanankonsultasi} />
           <Route path="/Informasi" Component={Informasi} />
           <Route path="/Kalkulator" Component={Kalkulator} />
-          <Route path="/Payment" Component={Payment} />
-          <Route path="/LoginPage" Component={LoginPage} />
-          <Route path="/SignUpPage" Component={SignUpPage}/>
           <Route path="/Tentangkami" Component={Tentangkami}/>
+          <Route path="/Payment" Component={Payment} />
+          <Route path="/LoginPage" Component={LoginSignUpPage} />
+          <Route path="/SignUpPage" Component={SignUpPage}/>
+          <Route path="/Profil" Component={Profil}/>
+          <Route path="/Chat" Component={Chat}/>
+          <Route path="/Keranjang" Component={Keranjang}/>
         </Routes>
 
-        {/* <FooterComponents/> */}
+        <FooterComponents/>
       </div>
   
       );
